40d70477c6faae3e1b4f3b3427a52e7f4570dfd7
Colin BERNARD <colin.bernard@ymail.com>
2023-12-04 12:52:59 +0100

    Shader test

--- Assets/Scripts/Voronoi.cs
+++ Assets/Scripts/Voronoi.cs
@@ -1,9 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class Voronoi : MonoBehaviour
 {
+    public Vector2Int imageDim;
+    public int regionAmount;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,21 @@ public class Voronoi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2Int[] centroids = GenerateDiagram();
+        }
+    }
+
+    Vector2Int[] GenerateDiagram()
+    {
+        Vector2Int[] centroids = new Vector2Int[regionAmount];
         
+        for (int i = 0; i < regionAmount; i++)
+        {
+            centroids[i] = new Vector2Int(Random.Range(0, imageDim.x), Random.Range(0, imageDim.y));
+        }
+
+        return centroids;
     }
 }
